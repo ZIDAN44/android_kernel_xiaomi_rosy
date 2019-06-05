75182d421912c2f22d8be5bba7ca464039e565c6
muralivijay <muralidharan9845@gmail.com>
2019-06-05 00:18:23 +0000

    Revert "rosy: sound: import oreo audio changes"
    This reverts commit 67b9a2a84cbf3374083a3364e6d308fdb23b10a2.

--- sound/usb/mixer.c
+++ sound/usb/mixer.c
@@ -2201,7 +2201,7 @@ static int parse_audio_unit(struct mixer_build *state, int unitid)
 static void snd_usb_mixer_free(struct usb_mixer_interface *mixer)
 {
 	/* kill pending URBs */
-	snd_usb_mixer_disconnect(mixer);
+	snd_usb_mixer_disconnect(&mixer->list);
 
 	kfree(mixer->id_elems);
 	if (mixer->urb) {
@@ -2534,8 +2534,11 @@ _error:
 	return err;
 }
 
-void snd_usb_mixer_disconnect(struct usb_mixer_interface *mixer)
+void snd_usb_mixer_disconnect(struct list_head *p)
 {
+	struct usb_mixer_interface *mixer;
+
+	mixer = list_entry(p, struct usb_mixer_interface, list);
 	if (mixer->disconnected)
 		return;
 	if (mixer->urb)
@@ -2623,4 +2626,3 @@ int snd_usb_mixer_resume(struct usb_mixer_interface *mixer, bool reset_resume)
 	return snd_usb_mixer_activate(mixer);
 }
 #endif
-
